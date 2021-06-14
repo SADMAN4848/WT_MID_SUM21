07336dc9bed6353e4518910ed76ac5c4d068843f
Md. Sadman Rahman <sadmanrman@gmail.com>
2021-06-14 11:09:34 +0600

    Edited Problem 5 of Lab 3

--- Lab3/Problem5.php
+++ Lab3/Problem5.php
@@ -1,9 +1,20 @@
 <?php
-$backColor=”green”; $fontColor=”red”; $imageWidth=”80px”; $imageHeight=”80px”;
-
-<img src=”aiub_logo.jpg” width=”300px” height=”300px” />
-
-<h2>American International University -<span style="background-color:$backColor;color:$fontColor">
-Bangladesh</h2>
-</span>
+$backColor="green";
+$fontColor="red";
+$imageWidth="80px";
+$imageHeight="80px";
 ?>
+
+<table>
+	<tr>
+		<td>
+			<img src ="aiub_logo.png" width="<?php echo $imageWidth; ?>" height="<?php echo $imageHeight;?>"/>
+		</td>
+		<td>
+			<h2>American International <br> University -<span style ="background-color:<?php echo $backColor;?>;color:$fontColor="red";">
+			Bangladesh</span></h2>
+		</td>
+	</tr>
+</table>
+
+
